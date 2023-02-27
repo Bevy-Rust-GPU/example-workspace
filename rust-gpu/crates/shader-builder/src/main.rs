3039b6429c5a518fa1c0ce2bfb4ea9f1b0e9504a
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-02-27 06:07:54 +0000

    Argument parsing for shader-builder

--- rust-gpu/crates/shader-builder/src/main.rs
+++ rust-gpu/crates/shader-builder/src/main.rs
@@ -1,4 +1,9 @@
-use std::{error::Error, path::Path};
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
+
+use clap::{error::ErrorKind, Parser};
 
 use futures::{
     channel::mpsc::{self, channel, Receiver, UnboundedSender},
@@ -8,12 +13,12 @@ use futures::{
 
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
+use spirv_builder::{MetadataPrintout, SpirvBuilder, SpirvMetadata};
 
 use tracing::{error, info};
 
-fn build_shader() {
-    SpirvBuilder::new("crates/shader", "spirv-unknown-spv1.5")
+fn build_shader(path_to_crate: impl AsRef<Path>, target: impl Into<String>) {
+    SpirvBuilder::new(path_to_crate, target)
         .print_metadata(MetadataPrintout::None)
         .build()
         .ok();
@@ -58,11 +63,87 @@ async fn async_watch<P: AsRef<Path>>(
     Ok(())
 }
 
+fn spirv_metadata(s: &str) -> Result<SpirvMetadata, clap::Error> {
+    match s {
+        "none" => Ok(SpirvMetadata::None),
+        "name-variables" => Ok(SpirvMetadata::NameVariables),
+        "full" => Ok(SpirvMetadata::Full),
+        _ => Err(clap::Error::new(ErrorKind::InvalidValue)),
+    }
+}
+
+#[derive(Debug, Clone, Parser)]
+#[command(author, version, about, long_about = None)]
+struct ShaderBuilder {
+    /// Shader crate to compile
+    path_to_crate: PathBuf,
+    /// rust-gpu compile target
+    #[arg(short, long, default_value = "spirv-unknown-spv1.5")]
+    target: String,
+    #[arg(long, default_value = "false")]
+    deny_warnings: bool,
+    #[arg(long, default_value = "true")]
+    release: bool,
+    #[arg(long, default_value = "false")]
+    multimodule: bool,
+    #[arg(long, value_parser=spirv_metadata, default_value = "none")]
+    spirv_metadata: SpirvMetadata,
+    #[arg(long, default_value = "false")]
+    relax_struct_store: bool,
+    #[arg(long, default_value = "false")]
+    relax_logical_pointer: bool,
+    #[arg(long, default_value = "false")]
+    relax_block_layout: bool,
+    #[arg(long, default_value = "false")]
+    uniform_buffer_standard_layout: bool,
+    #[arg(long, default_value = "false")]
+    scalar_block_layout: bool,
+    #[arg(long, default_value = "false")]
+    skip_block_layout: bool,
+    #[arg(long, default_value = "false")]
+    preserve_bindings: bool,
+    /// If set, will watch the provided path and recompile on change
+    #[arg(short, long)]
+    watch_path: Option<String>,
+}
+
+impl ShaderBuilder {
+    pub fn build_shader(&self) {
+        SpirvBuilder::new(&self.path_to_crate, &self.target)
+            .deny_warnings(self.deny_warnings)
+            .release(self.release)
+            .multimodule(self.multimodule)
+            .spirv_metadata(self.spirv_metadata)
+            .relax_struct_store(self.relax_struct_store)
+            .relax_logical_pointer(self.relax_logical_pointer)
+            .relax_block_layout(self.relax_block_layout)
+            .uniform_buffer_standard_layout(self.uniform_buffer_standard_layout)
+            .scalar_block_layout(self.scalar_block_layout)
+            .skip_block_layout(self.skip_block_layout)
+            .preserve_bindings(self.preserve_bindings)
+            .print_metadata(MetadataPrintout::None)
+            .build()
+            .ok();
+    }
+}
+
 fn main() {
     tracing_subscriber::fmt().init();
 
+    let args = ShaderBuilder::parse();
+
     println!();
     info!("Shader Builder");
+    println!();
+
+    info!("Building shader...");
+    build_shader(&args.path_to_crate, &args.target);
+    info!("Build complete!");
+    println!();
+
+    if args.watch_path.is_none() {
+        return;
+    };
 
     let pool = ThreadPool::new().expect("Failed to build pool");
     let (change_tx, mut change_rx) = mpsc::unbounded::<()>();
@@ -71,11 +152,17 @@ fn main() {
     let mut building = false;
 
     let fut_values = async move {
-        info!("Watching for changes...");
+        let mut args = args;
+
+        let Some(watch_path) = args.watch_path.take() else {
+            unreachable!();
+        };
+
+        info!("Watching {watch_path:} for changes...");
         println!();
         {
             pool.spawn_ok(async move {
-                async_watch(".", change_tx).await.unwrap();
+                async_watch(watch_path, change_tx).await.unwrap();
             });
         }
 
@@ -89,8 +176,9 @@ fn main() {
                         info!("Building shader...");
                         pool.spawn_ok({
                             let mut build_tx = build_tx.clone();
+                            let args = args.clone();
                             async move {
-                                build_shader();
+                                args.build_shader();
                                 build_tx.send(()).await.unwrap();
                             }
                         })
