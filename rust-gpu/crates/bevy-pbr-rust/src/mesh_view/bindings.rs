d44b1ffdead6ba63e291b6d40f24ac14f01d59b0
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-02-28 03:32:54 +0000

    Move bevy-rust-gpu, bevy-pbr-rust to separate repos

--- rust-gpu/crates/bevy-pbr-rust/src/mesh_view/bindings.rs
+++
@@ -1,8 +0,0 @@
-use spirv_std::Image;
-
-pub type TextureDepthCube = Image!(cube, type = f32, sampled = true, depth = true);
-pub type TextureDepthCubeArray =
-    Image!(cube, type = f32, sampled = true, depth = true, arrayed = true);
-
-pub type TextureDepth2d = Image!(2D, type = f32, sampled = true, depth = true);
-pub type TextureDepth2dArray = Image!(2D, type = f32, sampled = true, depth = true, arrayed = true);
--- rust-gpu/crates/bevy-pbr-rust/src/mesh_view/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod bindings;
-pub mod cluster_light_index_lists;
-pub mod cluster_offsets_and_counts;
-pub mod directional_light;
-pub mod lights;
-pub mod point_light;
-pub mod point_lights;
-pub mod view;
-pub mod globals;
